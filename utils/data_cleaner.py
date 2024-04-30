b9c9fbc0d4cdcca114ae13a693f5e3d187a6de6b
dev-abuke <abubekershamil123@gmail.com>
2024-04-30 12:30:47 +0300

    Refactor data_cleaner.py to improve file path handling and add method for reading lines

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -1,17 +1,20 @@
 import csv
 from itertools import islice
-from logger import Logger
+import os
+from logger import logger
 import sys
 class DataExtractor():
     
-    def __init__(self)->None:
+    def __init__(self, file_path: str)->None:
         try:
-            self.logger=Logger().get_app_logger()
-            self.logger.info('Data extractor object Initialized')
+            print("Initialized data extractor object")
+            self.file_path = file_path
+            # self.logger=logger()
+            # self.logger.info('Data extractor object Initialized')
         except:
             pass
     
-    def get_columns_and_rows(self, file_path: str) -> tuple:
+    def get_columns_and_rows(self) -> tuple:
         """
         Reads the first 100 rows of a file
 
@@ -22,7 +25,7 @@ class DataExtractor():
             tuple: A tuple containing the columns and rows of the file
         """
         try:
-            with open(f'../data/{file_path}', 'r', newline='') as f:
+            with open(f'data/{self.file_path}', 'r', newline='') as f:
                 reader = csv.reader(f, delimiter=';')
                 columns, rows = next(reader), list(islice(reader, 99))
             return columns, rows
@@ -34,3 +37,6 @@ class DataExtractor():
                 pass
             sys.exit(1)
     
+    def get_lines(self):
+        with open(f"data/{self.file_path}", 'r', buffering=100000000) as file:
+            return file.readlines()
