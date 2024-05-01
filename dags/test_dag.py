d33f26029a1da657433e50292f8aba9e550c13c3
dev-abuke <abubekershamil123@gmail.com>
2024-05-01 16:28:20 +0300

    Refactor test_dag.py to extract and load data to database

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -13,6 +13,7 @@ sys.path.append(f'./data')
 sys.path.insert(0,os.path.abspath(os.path.dirname(__file__)))
 
 from utils.data_cleaner import DataExtractor
+from utils.db_conn_loader import DatabaseLoader
 
 extractor = DataExtractor(file_name='20181024_d1_0830_0900.csv')
 
@@ -22,26 +23,57 @@ def extract_data():
     
     print(f"EXTRACTION : DONE EXTRACTING AND CLEANING DATA\nTRACKS: {df_track.shape} : TRAJECTORIES: {df_trajectory.shape}")
 
-def save_to_csv():
+def save_to_csv(ti):
 
     track_file_name, trajectory_file_name = extractor.save_to_csv()
 
     print(f"Data loaded to {track_file_name} and {trajectory_file_name}")
 
+    ti.xcom_push(key="track_file_name",value=track_file_name)
+    ti.xcom_push(key="trajectory_file_name",value=trajectory_file_name)
+
+def init_db_conn(ti):
+    
+    print("INITIALIZING DATABASE CONNECTION")
+
+    loader = DatabaseLoader()
+
+    loader.connect()
+
+    print("DATABASE CONNECTION INITIALIZED")
+
+    ti.xcom_push(key="db_loader_instance",value=loader)
+
+def load_data_to_db(ti):
+
+    print("LOADING DATA TO DATABASE")
+    
+    loader: DatabaseLoader = ti.xcom_pull(key="db_loader_instance",task_ids='initialize_db_connection')
+
+    track_file_name = ti.xcom_pull(key="track_file_name",task_ids='save_data_extracted_to_csv')
+
+    trajectory_file_name = ti.xcom_pull(key="trajectory_file_name",task_ids='save_data_extracted_to_csv')
+
+    loader.load_data_to_table(df=pd.read_csv(f'data/{track_file_name}.csv'), table_name=f'{track_file_name}')
+
+    loader.load_data_to_table(df=pd.read_csv(f'data/{trajectory_file_name}.csv'), table_name=f'{trajectory_file_name}')
+
+    print("DATA LOADED TO DATABASE")
+
 # Define the DAG
 dag = DAG(
-    'load_traffic_data',
+    'traffic_data_analytics_data_warehouse',
     default_args={
         'owner': 'airflow',
         'depends_on_past': False,
-        'start_date': datetime(year=2024, month=4, day=30),
+        'start_date': datetime(year=2024, month=4, day=30, hour=12, minute=0), #datetime(2024, 4, 30, 12, 0)
         'email_on_failure': False,
         'email_on_retry': False,
         'retries': 1,
         'retry_delay': timedelta(minutes=5),
     },
     description='Load automobile data into PostgreSQL',
-    schedule_interval=timedelta(seconds=60 * 60),
+    schedule_interval=timedelta(days=1), # No Schedule Run Only Once
     dagrun_timeout=timedelta(seconds=60),
 )
 
@@ -58,5 +90,19 @@ save_to_csv_task = PythonOperator(
     dag=dag,
 )
 
+# Define the task
+initialize_db = PythonOperator(
+    task_id='initialize_db_connection',
+    python_callable=init_db_conn,
+    dag=dag,
+)
+
+# Define the task
+load_to_db = PythonOperator(
+    task_id='load_data_to_db',
+    python_callable=load_data_to_db,
+    dag=dag,
+)
+
 # Define the pipeline 
-extract_data_task >> save_to_csv_task
+extract_data_task >> save_to_csv_task >> initialize_db >> load_to_db
