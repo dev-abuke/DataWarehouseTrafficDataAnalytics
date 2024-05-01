df4afe7fb9ed80094a8e38316adbc8b0c26eac1f
dev-abuke <abubekershamil123@gmail.com>
2024-05-01 21:58:32 +0300

    Refactor test_dag.py to extract and load data to database

--- dags/test_dag.py
+++ dags/test_dag.py
@@ -17,48 +17,45 @@ from utils.db_conn_loader import DatabaseLoader
 
 extractor = DataExtractor(file_name='20181024_d1_0830_0900.csv')
 
-def extract_data():
+loader = DatabaseLoader()
 
-    df_track, df_trajectory = extractor.extract_clean_data()
+def extract_data(ti):
+
+    df_track, df_trajectory, track_file_name, trajectory_file_name = extractor.extract_clean_data()
     
     print(f"EXTRACTION : DONE EXTRACTING AND CLEANING DATA\nTRACKS: {df_track.shape} : TRAJECTORIES: {df_trajectory.shape}")
-
-def save_to_csv(ti):
-
-    track_file_name, trajectory_file_name = extractor.save_to_csv()
-
-    print(f"Data loaded to {track_file_name} and {trajectory_file_name}")
 
     ti.xcom_push(key="track_file_name",value=track_file_name)
     ti.xcom_push(key="trajectory_file_name",value=trajectory_file_name)
 
-def init_db_conn(ti):
-    
-    print("INITIALIZING DATABASE CONNECTION")
+    print(f"DATA PUSH and EXTRACTION COMPLETED FILENAME {track_file_name} and {trajectory_file_name}")
 
-    loader = DatabaseLoader()
+def load_track_data_to_db(ti):
 
-    loader.connect()
+    print("LOADING TRACK DATA TO DATABASE")
 
-    print("DATABASE CONNECTION INITIALIZED")
+    track_file_name = ti.xcom_pull(key="track_file_name",task_ids='extract_data_from_raw_csv')
 
-    ti.xcom_push(key="db_loader_instance",value=loader)
+    loader.load_data_to_table(df=pd.read_csv(f'data/{track_file_name}.csv'), table_name='track_data')
 
-def load_data_to_db(ti):
+    print("TRACK DATA LOADED TO DATABASE")
 
-    print("LOADING DATA TO DATABASE")
-    
-    loader: DatabaseLoader = ti.xcom_pull(key="db_loader_instance",task_ids='initialize_db_connection')
 
-    track_file_name = ti.xcom_pull(key="track_file_name",task_ids='save_data_extracted_to_csv')
+def load_trajectory_data_to_db(ti):
 
-    trajectory_file_name = ti.xcom_pull(key="trajectory_file_name",task_ids='save_data_extracted_to_csv')
+    print("LOADING TRAJECTORY DATA TO DATABASE")
 
-    loader.load_data_to_table(df=pd.read_csv(f'data/{track_file_name}.csv'), table_name=f'{track_file_name}')
+    trajectory_file_name = ti.xcom_pull(key="trajectory_file_name",task_ids='extract_data_from_raw_csv')
 
-    loader.load_data_to_table(df=pd.read_csv(f'data/{trajectory_file_name}.csv'), table_name=f'{trajectory_file_name}')
+    loader.load_data_to_table(df=pd.read_csv(f'data/{trajectory_file_name}.csv'), table_name='trajectory_data')
 
-    print("DATA LOADED TO DATABASE")
+    print("TRAJECTORY DATA LOADED TO DATABASE")
+
+def create_table():
+    print("CREATE TABLE")
+
+def clean_up():
+    print("CREATE TABLE")
 
 # Define the DAG
 dag = DAG(
@@ -69,12 +66,11 @@ dag = DAG(
         'start_date': datetime(year=2024, month=4, day=30, hour=12, minute=0), #datetime(2024, 4, 30, 12, 0)
         'email_on_failure': False,
         'email_on_retry': False,
-        'retries': 1,
+        'retries': 2,
         'retry_delay': timedelta(minutes=5),
     },
     description='Load automobile data into PostgreSQL',
     schedule_interval=timedelta(days=1), # No Schedule Run Only Once
-    dagrun_timeout=timedelta(seconds=60),
 )
 
 # Define the task
@@ -83,26 +79,30 @@ extract_data_task = PythonOperator(
     python_callable=extract_data,
     dag=dag,
 )
-# Define the task
-save_to_csv_task = PythonOperator(
-    task_id='save_data_extracted_to_csv',
-    python_callable=save_to_csv,
+
+load_track_to_db = PythonOperator(
+    task_id='load_track_data_to_db',
+    python_callable=load_track_data_to_db,
     dag=dag,
 )
 
-# Define the task
-initialize_db = PythonOperator(
-    task_id='initialize_db_connection',
-    python_callable=init_db_conn,
+load_trajectory_to_db = PythonOperator(
+    task_id='load_trajectory_data_to_db',
+    python_callable=load_trajectory_data_to_db,
+    dag=dag,
+)
+    
+create_db_table = PythonOperator(
+    task_id='create_table',
+    python_callable = create_table,
     dag=dag,
 )
 
-# Define the task
-load_to_db = PythonOperator(
-    task_id='load_data_to_db',
-    python_callable=load_data_to_db,
+clean_up_data = PythonOperator(
+    task_id='clean_up_data',
+    python_callable = clean_up,
     dag=dag,
 )
 
 # Define the pipeline 
-extract_data_task >> save_to_csv_task >> initialize_db >> load_to_db
+[extract_data_task, create_db_table] >> load_track_to_db >> load_trajectory_to_db >> clean_up_data
