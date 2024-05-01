c21629c1baf8c4ca1a6f1009ab1e4a8ba8301dbc
dev-abuke <abubekershamil123@gmail.com>
2024-05-01 03:12:59 +0300

    Refactor data_cleaner.py to extract and clean data from file

--- utils/data_cleaner.py
+++ utils/data_cleaner.py
@@ -3,13 +3,15 @@ from itertools import islice
 import os
 from logger import logger
 import sys
+import pandas as pd
+import os
 class DataExtractor():
     
-    def __init__(self, file_path: str)->None:
+    def __init__(self, file_name: str)->None:
         try:
             # os.chdir("..")
             print("Initialized data extractor object")
-            self.file_path = file_path
+            self.file_name = file_name
             # self.logger=logger()
             # self.logger.info('Data extractor object Initialized')
         except:
@@ -26,7 +28,7 @@ class DataExtractor():
             tuple: A tuple containing the columns and rows of the file
         """
         try:
-            with open(f'data/{self.file_path}', 'r', newline='') as f:
+            with open(f'data/{self.file_name}', 'r', newline='') as f:
                 reader = csv.reader(f, delimiter=';')
                 columns, rows = next(reader), list(islice(reader, 99))
             return columns, rows
@@ -41,3 +43,36 @@ class DataExtractor():
     def get_lines(self):
         with open(f"data/{self.file_path}", 'r', buffering=100000000) as file:
             return file.readlines()
+    
+    def extract_clean_data(self):
+        
+        lines = self.get_lines()
+
+        lines_as_lists = [line.strip('\n').strip().strip(';').split(';') for line in lines]
+
+        cols = lines_as_lists.pop(0)
+
+        track_cols = cols[:4]
+
+        trajectory_cols = ['track_id'] + cols[4:]
+        
+        track_info = []
+        trajectory_info = []
+
+        for row in lines_as_lists:
+            track_id = row[0]
+
+            # add the first 4 values to track_info
+            track_info.append(row[:4]) 
+
+            remaining_values = row[4:]
+            # reshape the list into a matrix and add track_id
+            trajectory_matrix = [ [track_id] + remaining_values[i:i+6] for i in range(0,len(remaining_values),6)]
+            # add the matrix rows to trajectory_info
+            trajectory_info = trajectory_info + trajectory_matrix
+
+        df_track = pd.DataFrame(data=track_info, columns=track_cols)
+
+        df_trajectory = pd.DataFrame(data=trajectory_info, columns=trajectory_cols)
+
+        return df_track, df_trajectory
